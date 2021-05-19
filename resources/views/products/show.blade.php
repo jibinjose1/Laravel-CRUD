4f15c125d6a20a006fa17648c63b3c1af0a1564b
Jibin Jose <jibinjose173@gmail.com>
2021-05-19 15:48:59 +0530

    initial commit

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>  </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{route('products.index')}}" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <p>{{$product->name}}</p>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Cource</strong>
+                <p>{{$product->cource}}</p>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price</strong>
+                <p>{{$product->price}}</p>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date Created</strong>
+                <p>{{$product->created_at}}</p>
+            </div>
+        </div>
+    </div>
+@endsection
